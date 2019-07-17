49db078853794149c25324e9ac09dad5034661de
timchen0607 <a0963573232@gmail.com>
2019-07-17 17:04:18 +0800

    new page update

--- js/all.js
+++ js/all.js
@@ -1,1 +1,51 @@
-$(function() {});
+$(function() {
+  $(".nav_t").click(function(e) {
+    e.preventDefault();
+    $(".page_t")
+      .siblings()
+      .css("transform", "translateX(100%)");
+    $(".page_t").css("transform", "translateX(0%)");
+    $(".nav_btn").removeClass("active");
+    $(".nav_t").addClass("active");
+  });
+  $(".nav_a").click(function(e) {
+    e.preventDefault();
+    $(".page_t").css("transform", "translateX(-100%)");
+    $(".page_r,.page_s").css("transform", "translateX(100%)");
+    $(".page_a").css("transform", "translateX(0%)");
+    $(".nav_btn").removeClass("active");
+    $(".nav_a").addClass("active");
+  });
+  $(".nav_r").click(function(e) {
+    e.preventDefault();
+    $(".page_t,.page_a").css("transform", "translateX(-100%)");
+    $(".page_s").css("transform", "translateX(100%)");
+    $(".page_r").css("transform", "translateX(0%)");
+    $(".nav_btn").removeClass("active");
+    $(".nav_r").addClass("active");
+  });
+  $(".nav_s").click(function(e) {
+    e.preventDefault();
+    $(".page_s")
+      .siblings()
+      .css("transform", "translateX(-100%)");
+    $(".page_s").css("transform", "translateX(0%)");
+    $(".nav_btn").removeClass("active");
+    $(".nav_s").addClass("active");
+  });
+
+  $(".start button").click(function(e) {
+    e.preventDefault();
+    $(".work")
+      .siblings()
+      .fadeOut(500);
+    $(".work").fadeIn(500);
+  });
+  $(".work span,.break span").click(function(e) {
+    e.preventDefault();
+    $(".start")
+      .siblings()
+      .fadeOut(500);
+    $(".start").fadeIn(500);
+  });
+});
